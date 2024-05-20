6dba3668d4b98066ff2fe7b3f0172523ed97db8d
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-05-20 06:04:08 -0300

    update player/index.js

--- player/index.js
+++ player/index.js
@@ -9,24 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const legendUrl = params.get('legend');
 
     // Configurar o Plyr
-    const player = new Plyr('#video-player', { controls:
-  'play-large', // The large play button in the center
-  'restart', // Restart playback
-  'rewind', // Rewind by the seek time (default 10 seconds)
-  'play', // Play/pause playback
-  'fast-forward', // Fast forward by the seek time (default 10 seconds)
-  'progress', // The progress bar and scrubber for playback and buffering
-  'current-time', // The current time of playback
-  'duration', // The full duration of the media
-  'mute', // Toggle mute
-  'volume', // Volume control
-  'captions', // Toggle captions
-  'settings', // Settings menu
-  'pip', // Picture-in-picture (currently Safari only)
-  'airplay', // Airplay (currently Safari only)
-  'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
-  'fullscreen', // Toggle fullscreen
-    },
+    const player = new Plyr('#video-player', 
     {
       captions: { active: true, update: true, language: 'auto' },
       keyboard: { focused: true, global: true },
