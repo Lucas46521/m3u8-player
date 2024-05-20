9da38dcbcbb83b2e2cdccf3afcc5b39403da7436
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-05-20 12:11:45 -0300

    update player/index.js

--- player/index.js
+++ player/index.js
@@ -1,44 +1,28 @@
-// player/index.js
-
 document.addEventListener('DOMContentLoaded', function() {
-  // Função para carregar o vídeo e a legenda
-  function loadVideoAndSubtitle() {
-    // Obter parâmetros da URL
-    const params = new URLSearchParams(window.location.search);
-    const videoUrl = params.get('url');
-    const legendUrl = params.get('legend');
+      const player = new Plyr('#player');
 
-    // Configurar o Plyr
-    const player = new Plyr('#video-player', 
-    {
-      captions: { active: true, update: true, language: 'ct' },
-      keyboard: { focused: true, global: true },
+      const params = new URLSearchParams(window.location.search);
+      const videoUrl = decodeURIComponent(params.get('url'));
+      const legendUrl = decodeURIComponent(params.get('legend'));
+
+      if (videoUrl) {
+        player.source = {
+          type: 'video',
+          sources: [
+            {
+              src: videoUrl,
+              type: 'video/mp4',
+            },
+          ],
+          tracks: [
+            {
+              kind: 'captions',
+              label: 'Custom',
+              srclang: 'ct',
+              src: legendUrl,
+              default: true,
+            },
+          ],
+        };
+      }
     });
-
-    // Carregar vídeo
-    // Carregar legenda, se houver
-    if (legendUrl) {
-      player.source = {
-        type: 'video',
-        sources: [{ src: decodeURIComponent(videoUrl), type: 'video/mp4' }],
-        tracks: [
-          {
-            kind: 'captions',
-            label: 'Custom',
-            srclang: 'pt',
-            src: decodeURIComponent(legendUrl),
-            default: true,
-          },
-        ],
-      };
-    } else {
-      player.source = {
-        type: 'video',
-        sources: [{ src: decodeURIComponent(videoUrl), type: 'video/mp4' }],
-        tracks: [],
-      };
-    }
-  }
-
-  loadVideoAndSubtitle();
-});
