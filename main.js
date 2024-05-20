59ddb3dabfe8b97990d1750088cc03cf2563f210
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-05-20 04:51:25 -0300

    update main.js

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ $(window).on('load', function () {
     $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
     $('#play-btn').on('click', function () {
         localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
-        window.location.href = './player' + '#' + $('#m3u8-placeholder')[0].value;
+        
+        // Obtenha o link da legenda, se fornecido
+        var subtitleLink = $('#subtitle-placeholder').val();
+        var url = './player' + '#' + $('#m3u8-placeholder')[0].value;
+
+        // Se um link de legenda for fornecido, adicione-o à URL
+        if (subtitleLink) {
+            url += '&subtitle=' + subtitleLink;
+        }
+
+        window.location.href = url;
     });
 });
