212624310ea90b9be13d4a1d322292d18477e23a
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-05-20 04:48:58 -0300

    update player/player.js

--- player/player.js
+++ player/player.js
@@ -1,64 +1,21 @@
-var video = document.getElementById('video');
-
-function playM3u8(url){
-  if(Hls.isSupported()) {
-      video.volume = 0.3;
-      var hls = new Hls();
-      var m3u8Url = decodeURIComponent(url)
-      hls.loadSource(m3u8Url);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED,function() {
-        video.play();
-      });
-      document.title = url
-    }
-	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-		video.src = url;
-		video.addEventListener('canplay',function() {
-		  video.play();
-		});
-		video.volume = 0.3;
-		document.title = url;
-  	}
-}
-
-function playPause() {
-    video.paused?video.play():video.pause();
-}
-
-function volumeUp() {
-    if(video.volume <= 0.9) video.volume+=0.1;
-}
-
-function volumeDown() {
-    if(video.volume >= 0.1) video.volume-=0.1;
-}
-
-function seekRight() {
-    video.currentTime+=5;
-}
-
-function seekLeft() {
-    video.currentTime-=5;
-}
-
-function vidFullscreen() {
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen();
-  } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
-    }
-}
-
-playM3u8(window.location.href.split("#")[1])
 $(window).on('load', function () {
-    $('#video').on('click', function(){this.paused?this.play():this.pause();});
-    Mousetrap.bind('space', playPause);
-    Mousetrap.bind('up', volumeUp);
-    Mousetrap.bind('down', volumeDown);
-    Mousetrap.bind('right', seekRight);
-    Mousetrap.bind('left', seekLeft);
-    Mousetrap.bind('f', vidFullscreen);
+    $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
+    $('#play-btn').on('click', function () {
+        localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
+        window.location.href = './player' + '#' + $('#m3u8-placeholder')[0].value;
+    });
+
+    // Adicione esta parte para lidar com a legenda
+    $('#submit-subtitle-btn').on('click', function () {
+        var subtitleLink = $('#subtitle-placeholder').val();
+        // Se um link de legenda for fornecido, adicione-o ao vídeo
+        if (subtitleLink) {
+            var video = document.getElementById('video');
+            var track = document.createElement('track');
+            track.kind = 'subtitles';
+            track.src = subtitleLink;
+            track.label = 'Portuguese'; // Defina o rótulo da legenda aqui
+            video.appendChild(track);
+        }
+    });
 });
