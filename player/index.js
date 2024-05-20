d4d8a14600034e55e58c0bfa2eb0cc9e1d4da4e6
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-05-20 12:54:28 -0300

    update player/index.js

--- player/index.js
+++ player/index.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-      const player = new Plyr('#video-player', 
-      captions: { active: true, update: true, language: 'ct' },
-      keyboard: { focused: true, global: true },
-    });
+  const player = new Plyr('#video-player', {
+    captions: { active: true, update: true, language: 'ct' },
+    keyboard: { focused: true, global: true },
+  });
 
-      const params = new URLSearchParams(window.location.search);
-      const videoUrl = decodeURIComponent(params.get('url'));
-      const legendUrl = decodeURIComponent(params.get('legend'));
+  const params = new URLSearchParams(window.location.search);
+  const videoUrl = decodeURIComponent(params.get('url'));
+  const legendUrl = decodeURIComponent(params.get('legend'));
 
-      if (videoUrl) {
-        player.source = {
-          type: 'video',
-          sources: [
-            {
-              src: videoUrl,
-              type: 'video/mp4',
-            },
-          ],
-          tracks: [
-            {
-              kind: 'captions',
-              label: 'Custom',
-              srclang: 'ct',
-              src: legendUrl,
-              default: true,
-            },
-          ],
-        }
-      }
-    });
+  if (videoUrl) {
+    player.source = {
+      type: 'video',
+      sources: [
+        {
+          src: videoUrl,
+          type: 'video/mp4',
+        },
+      ],
+      tracks: [
+        {
+          kind: 'captions',
+          label: 'Custom',
+          srclang: 'ct',
+          src: legendUrl,
+          default: true,
+        },
+      ],
+    };
+  }
+});
