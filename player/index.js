e10befce9d008ee73101afd2f53750cdec9df710
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-05-20 12:48:16 -0300

    update player/index.js

--- player/index.js
+++ player/index.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
       const player = new Plyr('#video-player', 
-    
+      
+      tooltips: {
+        controls: true,
+      },
       captions: { active: true, update: true, language: 'ct' },
       keyboard: { focused: true, global: true },
       )
