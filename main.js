2648aec5494beda0cd5d6b973ff3b58e2d289e3b
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-05-20 05:00:47 -0300

    update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(window).on('load', function () {
         localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
         
         // Obtenha o link da legenda, se fornecido
-        var subtitleLink = $('#subtitle-placeholder').val();
+        var subtitleLink = $('#legenda-placeholder').val();
         var url = './player' + '#' + $('#m3u8-placeholder')[0].value;
 
         // Se um link de legenda for fornecido, adicione-o à URL
