6f26335cde7b3f29932b33606b06ae46aaeee7b8
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-05-20 15:57:03 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-// index.js na raiz do projeto
-
 document.addEventListener('DOMContentLoaded', function() {
   const playVideoBtn = document.getElementById('play-video');
 
   playVideoBtn.addEventListener('click', function() {
     const videoUrl = encodeURIComponent(document.getElementById('video-url').value);
     const legendUrl = encodeURIComponent(document.getElementById('legend-url').value);
-    const legendFile = encodeURIComponent(document.getElementById('legend-file').files[0]);
+    const legendFile = document.getElementById('legend-file').files[0];
+
+    let legendParam = '';
+
+    if (legendUrl) {
+      legendParam = `&legend=${legendUrl}`;
+    } else if (legendFile) {
+      const reader = new FileReader();
+      reader.onload = function(event) {
+        const legendData = event.target.result;
+        const legendDataEncoded = encodeURIComponent(legendData);
+        window.location.href = `player/?url=${videoUrl}&legend=${legendDataEncoded}`;
+      };
+      reader.readAsDataURL(legendFile);
+      return; // Prevent further execution
+    }
 
     // Redirecionar para a página do player com os argumentos na URL
-    window.location.href = `player/?url=${videoUrl}&legend=${legendUrl || legendFile}`;
+    window.location.href = `player/?url=${videoUrl}${legendParam}`;
   });
 });
