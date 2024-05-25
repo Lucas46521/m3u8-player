f0d9b960bdd0951bb0e43829d72d6ba570978a77
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-05-25 20:33:21 -0300

    update player/index.js

--- player/index.js
+++ player/index.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   const legendUrl = params.get('legend') ? decodeURIComponent(params.get('legend')) : '';
   const title = decodeURIComponent(params.get('title'));
 
-  // Initialize the video player
   var player = videojs('video-player', {
     responsive: true,
     autoplay: true,
@@ -13,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
     playbackRates: [0.5, 1, 1.5, 2],
     sources: [{
       src: videoUrl,
-      //type: 'video/mp4'
       type: 'application/x-mpegURL'
     }],
     tracks: [{
@@ -24,30 +22,25 @@ document.addEventListener('DOMContentLoaded', function() {
     }]
   });
 
-  // Check if httpSourceSelector plugin is available
   if (typeof player.httpSourceSelector === 'function') {
     player.httpSourceSelector();
   } else {
     console.warn('httpSourceSelector plugin is not loaded.');
   }
 
-  // Show loading spinner while video is loading
   player.on('waiting', function() {
     document.getElementById('loading-spinner').style.display = 'block';
   });
 
-  // Hide loading spinner when video can be played
   player.on('canplay', function() {
     document.getElementById('loading-spinner').style.display = 'none';
   });
 
-  // Hide loading screen and show player when video is ready
   player.on('ready', function() {
     document.getElementById('loading-screen').style.display = 'none';
     document.querySelector('.home-container').style.display = 'block';
   });
 
-  // Toggle fullscreen on double-click
   const videoElement = document.getElementById('video-player');
   videoElement.addEventListener('dblclick', function() {
     if (player.isFullscreen()) {
@@ -57,19 +50,16 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  // Show a custom error message
   player.on('error', function() {
     const errorDisplay = player.errorDisplay;
     errorDisplay.open();
     errorDisplay.contentEl().innerHTML = 'Erro ao carregar o vídeo. Por favor, tente novamente mais tarde.';
   });
 
-  // Set the video title, if available
   if (title) {
     document.getElementById('video-title').textContent = title;
   }
 
-  
   const skipBackwardButton = document.createElement('button');
   skipBackwardButton.textContent = '<< 10s';
   skipBackwardButton.className = 'vjs-skip-backward';
@@ -78,7 +68,6 @@ document.addEventListener('DOMContentLoaded', function() {
   };
   player.controlBar.el().appendChild(skipBackwardButton);
 
-  // Add skip forward and backward buttons
   const skipForwardButton = document.createElement('button');
   skipForwardButton.textContent = '>> 10s';
   skipForwardButton.className = 'vjs-skip-forward';
@@ -86,5 +75,4 @@ document.addEventListener('DOMContentLoaded', function() {
     player.currentTime(player.currentTime() + 10);
   };
   player.controlBar.el().appendChild(skipForwardButton);
-
- 
+});
